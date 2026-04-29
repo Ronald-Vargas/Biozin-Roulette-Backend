dcc87038a5f42a5239b9ca073b2c0f7e54709c86
Ronald-Vargas <ronald.vargasgo@gmail.com>
2026-04-29 17:27:31 -0600

    Initial Commit

---
+++ src/main/java/com/roulette/BiozinRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiozinRouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiozinRouletteApplication.class, args);
+    }
+
+}
